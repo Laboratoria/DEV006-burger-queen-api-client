fd7c693927f751cd6fe381601104fefe84fa1b79
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-07-07 22:48:09 -0600

    page not found

--- burger-queen/src/app/app.module.ts
+++ burger-queen/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
