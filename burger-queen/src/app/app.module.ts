7b7ded241a4573fce66d890c205b9c5ce6474734
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-07-11 12:14:54 -0600

    modulos

--- burger-queen/src/app/app.module.ts
+++ burger-queen/src/app/app.module.ts
@@ -1,37 +1,37 @@
 import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { NotFoundComponent } from './not-found/not-found/not-found.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from './menu/menu.component';
-import { LoginComponent } from './login/login.component';
+// import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu/menu.component';
+import { LoginComponent } from './login/login/login.component';
 
 //login
 //menu
 //pedidos
 //notFound
 
-const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full',
-  },
-  {
-    path: '**',
-    component: NotFoundComponent,
-  }
-]
+// const routes: Routes = [
+//   {
+//     path: 'login',
+//     component: LoginComponent,
+//   },
+//   {
+//     path: '',
+//     redirectTo: '/login',
+//     pathMatch: 'full',
+//   },
+//   {
+//     path: '**',
+//     component: NotFoundComponent,
+//   }
+// ]
 
 @NgModule({
   declarations: [
@@ -45,7 +45,7 @@ const routes: Routes = [
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes),
+    // RouterModule.forRoot(routes),
     HttpClientModule
   ],
   providers: [],
--- burger-queen/src/app/app.routing.module.ts
+++ burger-queen/src/app/app.routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: '',
+    component: LoginComponent,
+  }
+  // { path: '', redirectTo: 'login', pathMatch: 'full' },
   // { path: 'login', loadChildren: () => import ('./modules/login/login.module').then((m) => m.LoginModule) },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
