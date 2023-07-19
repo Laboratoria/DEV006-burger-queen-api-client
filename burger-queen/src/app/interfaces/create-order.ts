2ccb0baaa002b0fde26fa9821be6ec0328c34af9
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-07-19 11:29:16 -0600

    interfaces

--- burger-queen/src/app/interfaces/create-order.ts
+++ burger-queen/src/app/interfaces/create-order.ts
@@ -1,2 +1,12 @@
+import { MenuItem } from "./menuInterface";
+
 export interface CreateOrder {
+    userId: string;
+    client: string;
+    quantity?: number;
+    name: string;
+    price: number;
+    status: string;
+    dateEntry: any;
+    currentTime: any;
 }
